409dcb1242cebf03d7cb355d93c6d1f1a664dd4b
fali <fali@nisum.com>
2022-08-17 14:10:38 +0500

    created gaming project in angular

---
+++ src/app/interceptors/http-headers.interceptor.ts
@@ -0,0 +1,30 @@
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class HttpHeadersInterceptor implements HttpInterceptor {
+  constructor() {}
+
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    console.log('headers');
+    req = req.clone({
+      setHeaders: {
+        'x-rapidapi-key': 'esGbwrm390mshS2BCl0RALxQRtZTp1W7sFMjsnyJlJzDXVkW0H',
+        'x-rapidapi-host': 'rawg-video-games-database.p.rapidapi.com',
+      },
+      setParams: {
+        key: '1aa2b0844fe74d00b0eb8ca30dbcab1a',
+      },
+    });
+    return next.handle(req);
+  }
+}
